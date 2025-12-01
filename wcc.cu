6d2a18a52ac70563c93c938c5412724a5c85a923
lepakshikalyan <166225937+lepakshikalyan@users.noreply.github.com>
2025-12-01 13:37:12 +0530

    Update wcc.cu

--- wcc.cu
+++ wcc.cu
@@ -32,12 +32,13 @@ void buildCSR(int n,const vector<pair<int,int>>&edges,vector<int>&row_ptr,vector
     }
 }
 
-void loadGraph(const string&file,vector<tuple<int,int,int>>&edges,int&n){
+void loadGraph(const string&file,vector<pair<int,int>>&edges,int&n){
     ifstream fin(file);n=0;string line;
     while(getline(fin,line)){
         if(line.empty()||line[0]=='#')continue;
         stringstream ss(line);int u,v;ss>>u>>v;
-        edges.emplace_back(u,v,0);edges.emplace_back(v,u,0);
+        edges.push_back({u,v});
+        edges.push_back({v,u});
         n=max(n,max(u,v));
     }
     n++;
@@ -45,41 +46,43 @@ void loadGraph(const string&file,vector<tuple<int,int,int>>&edges,int&n){
 
 int main(){
     string graphFile="graph_95.txt",updateFile="updates_5.txt";
-    vector<tuple<int,int,int>>edges;int n;loadGraph(graphFile,edges,n);
 
-    vector<tuple<int,int,int>>updates;
-    {ifstream fin(updateFile);int u,v;while(fin>>u>>v){updates.emplace_back(u,v,0);updates.emplace_back(v,u,0);n=max(n,max(u,v));}}
+    vector<pair<int,int>>edges_orig;
+    int n;
+    loadGraph(graphFile,edges_orig,n);
 
-    int orig=edges.size()/2,upd=updates.size()/2;
-    bool skip=upd>0.20*orig;
+    vector<pair<int,int>>updates;
+    { ifstream fin(updateFile); int u,v; while(fin>>u>>v){ updates.push_back({u,v}); updates.push_back({v,u}); n=max(n,max(u,v)); } }
 
-    unordered_set<unsigned long long>seen;
-    vector<pair<int,int>>edges_after;
-    auto add_edge=[&](int u,int v){unsigned long long k=((unsigned long long)u<<32)|v;if(seen.insert(k).second)edges_after.push_back({u,v});};
-    for(auto&e:edges)add_edge(get<0>(e),get<1>(e));
-    for(auto&e:updates)add_edge(get<0>(e),get<1>(e));
-    sort(edges_after.begin(),edges_after.end());
+    vector<pair<int,int>>edges_after=edges_orig;
+    for(auto&e:updates)edges_after.push_back(e);
 
-    vector<int>row_ptr,col_idx;buildCSR(n,edges_after,row_ptr,col_idx);
+    vector<int>row_ptr0,col_idx0;
+    buildCSR(n,edges_orig,row_ptr0,col_idx0);
 
-    vector<int>comp(n);for(int i=0;i<n;i++)comp[i]=i;
+    vector<int>comp(n);
+    for(int i=0;i<n;i++)comp[i]=i;
 
     int*d_row,*d_col,*d_comp,*d_front,*d_next,*d_next_size;
     cudaMalloc(&d_row,(n+1)*sizeof(int));
-    cudaMalloc(&d_col,col_idx.size()*sizeof(int));
+    cudaMalloc(&d_col,col_idx0.size()*sizeof(int));
     cudaMalloc(&d_comp,n*sizeof(int));
     cudaMalloc(&d_front,n*sizeof(int));
-    cudaMalloc(&d_next,col_idx.size()*sizeof(int));
+    cudaMalloc(&d_next,col_idx0.size()*sizeof(int));
     cudaMalloc(&d_next_size,sizeof(int));
 
-    cudaMemcpy(d_row,row_ptr.data(),(n+1)*sizeof(int),cudaMemcpyHostToDevice);
-    cudaMemcpy(d_col,col_idx.data(),col_idx.size()*sizeof(int),cudaMemcpyHostToDevice);
+    cudaMemcpy(d_row,row_ptr0.data(),(n+1)*sizeof(int),cudaMemcpyHostToDevice);
+    cudaMemcpy(d_col,col_idx0.data(),col_idx0.size()*sizeof(int),cudaMemcpyHostToDevice);
     cudaMemcpy(d_comp,comp.data(),n*sizeof(int),cudaMemcpyHostToDevice);
 
-    cudaEvent_t s1,e1,s2,e2;cudaEventCreate(&s1);cudaEventCreate(&e1);cudaEventCreate(&s2);cudaEventCreate(&e2);
+    vector<int>front(n);
+    iota(front.begin(),front.end(),0);
+    int fsize=n;
+    cudaMemcpy(d_front,front.data(),n*sizeof(int),cudaMemcpyHostToDevice);
 
-    vector<int>frontier(n);iota(frontier.begin(),frontier.end(),0);
-    int fsize=n;cudaMemcpy(d_front,frontier.data(),n*sizeof(int),cudaMemcpyHostToDevice);
+    cudaEvent_t s1,e1,s2,e2;
+    cudaEventCreate(&s1);cudaEventCreate(&e1);
+    cudaEventCreate(&s2);cudaEventCreate(&e2);
 
     cudaEventRecord(s1);
     int MAX_ITERS=n;
@@ -96,44 +99,48 @@ int main(){
 
     cudaMemcpy(comp.data(),d_comp,n*sizeof(int),cudaMemcpyDeviceToHost);
 
-    unordered_map<int,int>mp;int maxi=0;
-    for(int i=0;i<n;i++){mp[comp[i]]++;maxi=max(maxi,mp[comp[i]]);}
-    cout<<"StaticLargest="<<maxi<<"\n";
+    unordered_map<int,int>mp;int static_max=0;
+    for(int i=0;i<n;i++){mp[comp[i]]++;static_max=max(static_max,mp[comp[i]]);}
+    cout<<"StaticLargest="<<static_max<<"\n";
 
-    vector<int>comp_cpu(n);for(int i=0;i<n;i++)comp_cpu[i]=i;
+    vector<int>comp_cpu(n);
+    for(int i=0;i<n;i++)comp_cpu[i]=i;
+
     bool ch=true;
-    auto cpu_start=chrono::high_resolution_clock::now();
+    auto cpu_s=chrono::high_resolution_clock::now();
     while(ch){
         ch=false;
         for(auto&e:edges_after){
-            int u=e.first,v=e.second,cu=comp_cpu[u],cv=comp_cpu[v],m=min(cu,cv);
+            int u=e.first,v=e.second;
+            int cu=comp_cpu[u],cv=comp_cpu[v];
+            int m=min(cu,cv);
             if(cu>m){comp_cpu[u]=m;ch=true;}
             if(cv>m){comp_cpu[v]=m;ch=true;}
         }
         for(int i=0;i<n;i++)while(comp_cpu[i]!=comp_cpu[comp_cpu[i]])comp_cpu[i]=comp_cpu[comp_cpu[i]];
     }
-    auto cpu_end=chrono::high_resolution_clock::now();
-    double cpu_time=chrono::duration<double,chrono::milliseconds::period>(cpu_end-cpu_start).count();
+    auto cpu_e=chrono::high_resolution_clock::now();
+    double cpu_time=chrono::duration<double,chrono::milliseconds::period>(cpu_e-cpu_s).count();
 
-    unordered_map<int,int>cpu_map;int cpu_max=0;
-    for(int i=0;i<n;i++){cpu_map[comp_cpu[i]]++;cpu_max=max(cpu_max,cpu_map[comp_cpu[i]]);}
+    unordered_map<int,int>cpu_mp;int cpu_max=0;
+    for(int i=0;i<n;i++){cpu_mp[comp_cpu[i]]++;cpu_max=max(cpu_max,cpu_mp[comp_cpu[i]]);}
     cout<<"CpuLargest="<<cpu_max<<"\n";
 
-    if(skip){
-        cout<<"DynamicSkipped\n";
-        cout<<"StaticTime="<<static_time<<"ms\n";
-        cout<<"Speedup="<<cpu_time/static_time<<"x\n";
-        cudaFree(d_row);cudaFree(d_col);cudaFree(d_comp);
-        cudaFree(d_front);cudaFree(d_next);cudaFree(d_next_size);
-        cudaDeviceReset();return 0;
-    }
+    vector<int>row_ptr1,col_idx1;
+    buildCSR(n,edges_after,row_ptr1,col_idx1);
 
-    cudaMemcpy(d_comp,comp.data(),n*sizeof(int),cudaMemcpyHostToDevice);
+    cudaMemcpy(d_row,row_ptr1.data(),(n+1)*sizeof(int),cudaMemcpyHostToDevice);
+    cudaMemcpy(d_col,col_idx1.data(),col_idx1.size()*sizeof(int),cudaMemcpyHostToDevice);
 
-    vector<int>dyn_frontier;
-    for(auto&e:updates)dyn_frontier.push_back(get<1>(e));
-    fsize=dyn_frontier.size();
-    cudaMemcpy(d_front,dyn_frontier.data(),fsize*sizeof(int),cudaMemcpyHostToDevice);
+    vector<int>comp_dyn(n);
+    for(int i=0;i<n;i++)comp_dyn[i]=comp[i];
+    cudaMemcpy(d_comp,comp_dyn.data(),n*sizeof(int),cudaMemcpyHostToDevice);
+
+    vector<int>dyn_front;
+    dyn_front.reserve(updates.size());
+    for(auto&e:updates)dyn_front.push_back(e.second);
+    fsize=dyn_front.size();
+    cudaMemcpy(d_front,dyn_front.data(),fsize*sizeof(int),cudaMemcpyHostToDevice);
 
     cudaEventRecord(s2);
     for(int it=0;it<MAX_ITERS&&fsize>0;it++){
@@ -147,21 +154,19 @@ int main(){
     cudaEventRecord(e2);cudaEventSynchronize(e2);
     float dynamic_time;cudaEventElapsedTime(&dynamic_time,s2,e2);
 
-    cudaMemcpy(comp.data(),d_comp,n*sizeof(int),cudaMemcpyDeviceToHost);
-    for(int i=0;i<n;i++)while(comp[i]!=comp[comp[i]])comp[i]=comp[comp[i]];
+    cudaMemcpy(comp_dyn.data(),d_comp,n*sizeof(int),cudaMemcpyDeviceToHost);
+    for(int i=0;i<n;i++)while(comp_dyn[i]!=comp_dyn[comp_dyn[i]])comp_dyn[i]=comp_dyn[comp_dyn[i]];
 
-    unordered_map<int,int>mps;maxi=0;
-    for(int i=0;i<n;i++){mps[comp[i]]++;maxi=max(maxi,mps[comp[i]]);}
+    unordered_map<int,int>mpd;int dyn_max=0;
+    for(int i=0;i<n;i++){mpd[comp_dyn[i]]++;dyn_max=max(dyn_max,mpd[comp_dyn[i]]);}
 
-    cout<<"DynamicLargest="<<maxi<<"\n";
-    cout<<"DynamicComp="<<mps.size()<<"\n";
+    cout<<"DynamicLargest="<<dyn_max<<"\n";
+    cout<<"DynamicComp="<<mpd.size()<<"\n";
     cout<<"StaticTime="<<static_time<<"ms\n";
     cout<<"DynamicTime="<<dynamic_time<<"ms\n";
-    cout<<"Speedup="<<cpu_time/dynamic_time<<"x\n";
+    cout<<"Speedup="<<static_time/dynamic_time<<"x\n";
 
-    cudaFree(d_row);cudaFree(d_col);cudaFree(d_comp);
-    cudaFree(d_front);cudaFree(d_next);cudaFree(d_next_size);
+    cudaFree(d_row);cudaFree(d_col);cudaFree(d_comp);cudaFree(d_front);cudaFree(d_next);cudaFree(d_next_size);
     cudaDeviceReset();
     return 0;
 }
-
